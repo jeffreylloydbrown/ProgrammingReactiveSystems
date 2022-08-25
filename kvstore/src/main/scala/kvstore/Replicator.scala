a9452390edd423c84bccf60a14c39378df252756
Jeff Brown <jeff_brown2@msn.com>
2022-08-25 17:14:43 -0500

    Replicator implemented including timeout for SnapshotAcks.  Step3 tests all pass.

--- kvstore/src/main/scala/kvstore/Replicator.scala
+++ kvstore/src/main/scala/kvstore/Replicator.scala
@@ -1,8 +1,6 @@
 package kvstore
 
-import akka.actor.Props
-import akka.actor.Actor
-import akka.actor.ActorRef
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, ReceiveTimeout}
 import akka.event.LoggingReceive
 
 import scala.concurrent.duration._
@@ -10,26 +8,21 @@ import scala.concurrent.duration._
 object Replicator {
   case class Replicate(key: String, valueOption: Option[String], id: Long)
   case class Replicated(key: String, id: Long)
-  
+
   case class Snapshot(key: String, valueOption: Option[String], seq: Long)
   case class SnapshotAck(key: String, seq: Long)
 
   def props(replica: ActorRef): Props = Props(new Replicator(replica))
-}
 
-class Replicator(val replica: ActorRef) extends Actor {
+  private val SnapshotTimeout = 100.milliseconds
+} // object Replicator
+
+class Replicator(val replica: ActorRef) extends Actor with ActorLogging {
   import Replicator._
-  import context.dispatcher
-  
-  /*
-   * The contents of this actor is just a suggestion, you can implement it in any way you like.
-   */
 
   // map from sequence number to pair of sender and request
-  var acks = Map.empty[Long, (ActorRef, Replicate)]
-  // a sequence of not-yet-sent snapshots (you can disregard this if not implementing batching)
-  var pending = Vector.empty[Snapshot]
-  
+  var awaitingSnapshotAcks = Map.empty[Long, (ActorRef, Replicate)]
+
   var _seqCounter = 0L
   private def nextSeq() = {
     val ret = _seqCounter
@@ -37,10 +30,37 @@ class Replicator(val replica: ActorRef) extends Actor {
     ret
   }
 
-  
-  /* TODO Behavior for the Replicator. */
-  def receive: Receive = LoggingReceive {
-    case _ =>
-  }
+  // These routines encapsulate setting and resetting our timeout.  Setting is straight-forward.  When we reset,
+  // what we do depends on us waiting on SnapshotAcks to come back.  If there are none, then we don't need the
+  // timeout anymore and we turn it off.  If there are SnapshotAcks still left to come, we keep the timeout in
+  // place.  Since the timeout continues until deactivated, we do nothing if there are still SnapshotAcks coming.
+  private def setTimeout(): Unit = context.setReceiveTimeout(SnapshotTimeout)
+  private def resetTimeout(): Unit =
+    if (awaitingSnapshotAcks.isEmpty) context.setReceiveTimeout(Duration.Undefined)
 
-}
+
+  def receive: Receive = LoggingReceive {
+    case request @ Replicate(key: String, value: Option[String], _) =>
+      val mySeqNumber = nextSeq()
+      awaitingSnapshotAcks += (mySeqNumber -> (sender(), request))
+      replica ! Snapshot(key, value, mySeqNumber)
+      setTimeout()
+
+    case SnapshotAck(_, seq: Long) =>
+      awaitingSnapshotAcks.get(seq).foreach {
+        case (theSender: ActorRef, replicate: Replicate) =>
+          theSender ! Replicated(replicate.key, replicate.id)
+      }
+      awaitingSnapshotAcks -= seq
+      resetTimeout()
+
+    case ReceiveTimeout =>
+      awaitingSnapshotAcks.foreach {
+        case (seqNumber, (_, request)) =>
+          replica ! Snapshot(request.key, request.valueOption, seqNumber)
+      }
+      resetTimeout()
+
+  } // Replicator Receive handler
+
+} // class Replicator
