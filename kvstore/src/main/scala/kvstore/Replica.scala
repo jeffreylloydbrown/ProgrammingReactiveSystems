a6cbaead8ad658fdf95caf3e662e85c228cb46aa
Jeff Brown <jeff_brown2@msn.com>
2022-09-26 16:32:39 -0500

    Implement Persist and Persisted, which required most of the support for Replicate.  Step4-case1 passing.

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -10,6 +10,10 @@
 // separate Receive handlers for larger-grained operations, and mix and match them to implement
 // the leader and secondary handlers.  I rather like how this separates the cases into highly-related
 // items, and lets one focus on just those cases instead of these gigantic Receive partial functions.
+// The only side effect is that you get excess "unhandled message" debug log entries for messages
+// that aren't covered by the first group of partial functions.  The important thing is to see that
+// eventually the message with the particular id/seq number is handled, and not let those log entries
+// bother you.
 
 package kvstore
 
@@ -41,18 +45,92 @@ object Replica {
 class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   import Replica._
   import Replicator._
+  import akka.actor.SupervisorStrategy._
   import Persistence._
   import context.dispatcher
+
+  override val supervisorStrategy: SupervisorStrategy = OneForOneStrategy(withinTimeRange = 1.second) {
+    case _: Exception =>
+      Restart
+  }
+
+  private def createPersistence = {
+    val persistence = context.actorOf(persistenceProps)
+    context.watch(persistence)
+    persistence
+  }
+
+  private var persistence = createPersistence
 
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
   
-  var kv = Map.empty[String, String]
+  private var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
-  var secondaries = Map.empty[ActorRef, ActorRef]
+  private var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
-  var replicators = Set.empty[ActorRef]
+  private var replicators = Set.empty[ActorRef]
+
+  private case class PendingOperation(notifyWhenDone: ActorRef,
+                                      messageIfSucceeded: Option[Any],
+                                      messageIfFailed: Option[Any])
+  private var pendingOperations = Map.empty[Long, PendingOperation]
+  private def addPendingOperation(id: Long, notifyWhenDone: ActorRef,
+                                  messageIfSucceeded: Option[Any],
+                                  messageIfFailed: Option[Any]): Unit = {
+    pendingOperations +=   id -> PendingOperation(notifyWhenDone, messageIfSucceeded, messageIfFailed)
+  }
+  private def removePendingOperation(id: Long)(getMessage: PendingOperation => Option[Any]): Unit = {
+    for (pendingOperation <- pendingOperations.get(id);
+         message <- getMessage(pendingOperation)) {
+      pendingOperation.notifyWhenDone ! message
+    }
+    pendingOperations -= id
+  }
+
+  private case class PendingReplicate(message: Any, stillWaitingOn: Set[ActorRef]) {
+    def anActorFinished(actor: ActorRef): PendingReplicate = this.copy(stillWaitingOn = stillWaitingOn - actor)
+  }
+  private var pendingReplicates = Map.empty[Long, PendingReplicate]
+  private def addPendingReplicate(id: Long, replicate: Replicate): Unit = {
+    // Only do this if there are any secondaries, because Leader doesn't have a Replicator.
+    if (secondaries.nonEmpty) {
+      val replicators = secondaries.values.toSet
+      pendingReplicates +=    id -> PendingReplicate(replicate, replicators)
+      replicators.foreach(_ ! replicate)
+    }
+  }
+  private def removePendingReplicate(id: Long, finished: ActorRef): Unit = {
+    pendingReplicates
+      .get(id)
+      .map { _.anActorFinished(finished) }  // stillWaitingOn becomes Set.empty if all done.
+      .foreach {
+        case pr: PendingReplicate if pr.stillWaitingOn.isEmpty =>
+          // all the replicators have finished so we are done replicating
+          pendingReplicates -= id
+        case replicationNotAllFinished: PendingReplicate =>
+          // we are still awaiting some replication acknowledgements, so update the state for this id.
+          pendingReplicates.updated(id, replicationNotAllFinished)
+      }
+  }
+
+  private case class PendingPersist(notifyWhenDone: ActorRef, message: Persist)
+  private var pendingPersists = Map.empty[Long, PendingPersist]
+  private def addPendingPersist(notifyWhenDone: ActorRef, seq: Long, persist: Persist): Unit = {
+    pendingPersists +=    seq -> PendingPersist(notifyWhenDone, persist)
+    persistence ! persist
+  }
+  private def removePendingPersist(seq: Long): Unit = { pendingPersists -= seq }
+
+  private def tryToFinishOperation(id: Long): Unit = {
+    (pendingReplicates.get(id), pendingPersists.get(id)) match {
+      case (None, None) =>
+        removePendingOperation(id)(_.messageIfSucceeded)
+      case _ =>
+      // not totally finished, so don't do anything
+    }
+  }
 
   def receive: Receive = LoggingReceive {
     case JoinedPrimary   => context.become(leader)
@@ -67,10 +145,16 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   private val Updates: Receive = LoggingReceive {
     case Insert(key: String, value: String, id: Long) =>
       kv += key->value
-      sender() ! OperationAck(id)
+      addPendingOperation(id, sender(),
+        Some(OperationAck(id)), Some(OperationFailed(id)))
+      addPendingReplicate(id, Replicate(key, Some(value), id))
+      addPendingPersist(self, id, Persist(key, Some(value), id))
     case Remove(key: String, id: Long) =>
       kv -= key
-      sender() ! OperationAck(id)
+      addPendingOperation(id, sender(),
+        Some(OperationAck(id)), Some(OperationFailed(id)))
+      addPendingReplicate(id, Replicate(key, None, id))
+      addPendingPersist(self, id, Persist(key, None, id))
   }
 
   private var expectedSeqId = 0L
@@ -87,13 +171,28 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
         case Some(v) => kv += key->v
         case None => kv -= key
       }
-      sender() ! SnapshotAck(key, seq)
+      addPendingOperation(seq, sender(),
+        Some(SnapshotAck(key, seq)), None)
+      addPendingPersist(sender(), seq, Persist(key, value, seq))
       expectedSeqId = seq + 1
+  }
+
+  private case object SendPendingPersists
+
+  private val Persists: Receive = LoggingReceive {
+    case Persisted(_, seq) =>
+      removePendingPersist(seq)
+      tryToFinishOperation(seq)
+    case SendPendingPersists =>
+      pendingPersists.foreach { case (_, persist) => persistence ! persist }
+    case Terminated(actor) if actor == persistence =>
+      persistence = createPersistence
+      self ! SendPendingPersists
   }
 
   // Here is where Chain of Responsibility comes in handy!
   private val leader: Receive = Gets orElse Updates
-  private val replica: Receive = Gets orElse Snapshots
+  private val replica: Receive = Gets orElse Snapshots orElse Persists
 
   // leave this at the absolute bottom so it happens last
   arbiter ! Join
