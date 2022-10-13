daaa47c70837aa2aaacafb6aa9d4da40ba5968b0
Jeff Brown <jeff_brown2@msn.com>
2022-10-13 11:45:49 -0500

    Add a reintroduceOrdering test case to confirm (incorrectly) reusing a sequence number is ignored.

--- followers/src/test/scala/followers/FollowersSuite.scala
+++ followers/src/test/scala/followers/FollowersSuite.scala
@@ -13,7 +13,6 @@ import followers.model.{Event, Identity}
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
-import scala.util.Random
 
 class FollowersSuite extends munit.FunSuite with TestKitBase {
   implicit lazy val system: ActorSystem = ActorSystem("FollowersSuite")
@@ -77,6 +76,19 @@ class FollowersSuite extends munit.FunSuite with TestKitBase {
     val sorted = Source(incoming) via reintroduceOrdering
     val got = await(sorted.runWith(Sink.seq))
     assertEquals(got, incoming.sortBy(_.sequenceNr))
+  }
+
+  test("reintroduceOrdering: reusing a sequence number doesn't affect the pipeline") {
+    val incoming = List(
+      Event.Follow(2, 1, 2),
+      Event.Follow(1, 1, 3),
+      Event.Follow(3, 1, 4),
+      Event.Follow(1, 2, 3)  // shouldn't appear in results
+    )
+
+    val sorted = Source(incoming) via reintroduceOrdering
+    val got = await(sorted.runWith(Sink.seq))
+    assertEquals(got, incoming.filterNot(_.fromUserId == 2).sortBy(_.sequenceNr))
   }
 
 
